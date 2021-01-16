214c86140f2d8468b1e39ad42ee436a10a10530f
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2021-01-16 16:17:45 +1000

    made some changes to main.rb

--- src/main.rb
+++ src/main.rb
@@ -13,29 +13,6 @@ welcome
 puts "\n   Hello There! \n\n"
 puts "Please enter your name: \n\n"
 
-# Creating a loop to make sure the user types in something. This exits out of the loop if the user does not type anything in after 3 tries. 
- 
-# user_name_count = 0
-# while user_name_count < 3
-#     user_name = gets.chomp.capitalize
-
-#     # exits the loop if the user types in a name
-#     if user_name != "" 
-#         clear
-#         break
-
-#     # continues the loop until the user types in a name, or until the user_name_count is greater than 3. 
-#     else
-#         user_name_count += 1
-#         if user_name_count == 3
-#             clear
-#             puts "\n\n 👀👀👀.."
-#             user_name = "coconut head 1"
-#         else
-#             puts "Ooops, please enter your name:"
-#         end
-#     end
-# end
     if ARGV.length > 0 
         user_name = ARGV[0]
     else
